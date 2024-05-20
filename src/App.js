5084691917bd96659a9432f71a6f1878586d2d5d
Robby Lewis <Robbysim37@gmail.com>
2024-05-20 15:29:54 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import './App.css';
+
+function App() {
+
+  const drivers = [
+    {name:"Robby", isStick:false, isCDL:false},
+    {name:"Alex", isStick:true, isCDL:false},
+    {name:"Anthony", isStick:false, isCDL:false},
+    {name:"Kenny", isStick:true, isCDL:true},
+    {name:"Ed", isStick:false, isCDL:true},
+    {name:"Dane", isStick:true, isCDL:false},
+    {name:"greg", isStick:true, isCDL:false},
+    {name:"chuck", isStick:false, isCDL:false}
+  ]
+
+  const keys = [
+    {vin:"pfa00055",rank:"standard"},
+    {vin:"pfa00056",rank:"CDL"},
+    {vin:"pfa00057",rank:"stick"},
+    {vin:"pfa00058",rank:"standard"},
+    {vin:"pfa00059",rank:"CDL"},
+    {vin:"pfa00060",rank:"stick"},
+    {vin:"pfa00061",rank:"CDL"},
+    {vin:"pfa00062",rank:"stick"}
+  ]
+
+  const unassignedKeys = []
+
+  const assignedKeys = []
+
+  const sortKeys = () => {
+    keys.sort((a,b) => {
+      if(a.rank === "CDL"){
+        return -1
+      }else if(a.rank === "stick" && b.rank !== "CDL"){
+        return -1
+      }else if(a.rank === "standard"){
+        return + 1
+      }
+    })
+  }
+
+  const assignKeys = () => {
+    keys.forEach(key => {
+      let currDriver
+      if(key.rank = "CDL"){
+        currDriver = drivers.find(driver => {
+          if(driver.isCDL){
+            return true
+          }
+        })
+        if(currDriver){
+          assignedKeys.push({
+            vin:key.vin,
+            rank:key.rank,
+            driver:currDriver
+          })
+
+        }
+      }else if(key.rank = "stick"){
+
+      }else if(key.rank = "standard"){
+
+      }else{
+
+      }
+    })
+  }
+
+  sortKeys()
+
+  return (
+    <div></div>
+  );
+}
+
+export default App;
