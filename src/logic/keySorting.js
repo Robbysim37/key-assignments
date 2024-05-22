747dfae85ea1589589c018bba274f7f0e0aca823
Robby Lewis <Robbysim37@gmail.com>
2024-05-22 13:50:34 -0400

    cleaned up wet code and big files. CSV parser working

---
+++ src/logic/keySorting.js
@@ -0,0 +1,60 @@
+
+// yes this should probably be a class but I was paid for "Quick and dirty."
+
+const CDLKeys = []
+const chauffeurKeys = []
+const manualKeys = []
+const standardKeys = []
+
+export const sortKeys = (keys) => {
+    keys.forEach(key => {
+      switch (key.rank){
+        case "Chauffeur":
+          chauffeurKeys.push(key)
+        break;
+        case "CDL":
+          CDLKeys.push(key)
+        break;
+        case "Manual":
+          manualKeys.push(key)
+        break;
+        default:
+          standardKeys.push(key)
+      }
+    })
+  }
+
+  const assignRank = (drivers,key) => {
+    let validDriver = drivers.find(driver => driver[key.rank] && driver.available)
+        if(validDriver){
+            validDriver.available = false
+            return {...key,name:validDriver.name}
+        }else{
+            return {...key,name:"No Driver"}
+        }
+  }
+
+export const assignKeys = (drivers) => {
+    const allKeys = [
+      CDLKeys,
+      chauffeurKeys,
+      manualKeys,
+      standardKeys
+    ]
+    const assignedKeys = []
+    const unassignedKeys = []
+
+    for(let i = 0; i < allKeys.length; i++){
+      allKeys[i].forEach(key => {
+        let keyDriverPair = assignRank(drivers,key)
+        keyDriverPair.name !== "No Driver" ? 
+        assignedKeys.push(keyDriverPair) 
+        : 
+        unassignedKeys.push(keyDriverPair)
+      })
+    }
+    return {
+        assignedKeys,
+        unassignedKeys
+    }
+  }
